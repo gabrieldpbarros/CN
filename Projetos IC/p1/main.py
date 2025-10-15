7f75030a299e204a82a6e9da34eb22b8b502bf7f
Gabriel D. <gabrielpanovich@gmail.com>
2025-10-15 01:04:09 -0300

    deleted:    .vscode/settings.json 	modified:   p1/__pycache__/methods.cpython-313.pyc 	modified:   p1/main.py 	modified:   p1/methods.py 	new file:   p1/p1.ipynb 	modified:   ../.gitignore

--- Projetos IC/p1/main.py
+++ Projetos IC/p1/main.py
@@ -1,18 +1,21 @@
-import math as m
+import numpy as np
 import pandas as pd
 
 from methods import Function
 
 def main():
-    f = lambda x: m.cos(x) - m.sin(x)
+    f = lambda x: np.cos(x, dtype="float16") - np.sin(x, dtype="float16")
+    fd = lambda x: -1 * (np.sin(x, dtype="float16") + np.cos(x, dtype="float16"))
     x0 = 0.0
-    x1 = m.pi / 2
+    x1 = np.pi / 2
     eps = 0.01
-    maxit = 7
-    func = Function(f, x0, x1, eps, maxit)
+    maxit = 10
+    func = Function(f, x0, x1, eps, maxit=maxit, derivative=fd)
     app, df = func.bisect()
-    print(f"Aproximacao = {app}")
-    print(f"DATAFRAME:\n{df}")
+    func.printFormated(df)
+    print(f"\nAproximacao = {app}")
+    print(df["f(xk)"][df.last_valid_index()])
+
 
 if __name__ == "__main__":
     main()
--- Projetos IC/p1/methods.py
+++ Projetos IC/p1/methods.py
@@ -4,7 +4,7 @@ import pandas as pd
 from typing import Callable, List, Tuple
 
 class Function:
-    def __init__(self, func: Callable, x0: float, x1: float, epsilon: float, maxit: int=20, derivative: Callable=None):
+    def __init__(self, func: Callable, x0: np.float16, x1: np.float16, epsilon: np.float16, maxit: int=20, derivative: Callable=None):
         """
         ARGS:
             func = Função analisada **(deve ser passada como função lambda)**
@@ -29,47 +29,92 @@ class Function:
             title = Título da tabela
             values = Valores da tabela
         """
-        pd.options.display.float_format = '{:.5f}'.format
+        pd.options.display.float_format = '{:.5E}'.format
         return pd.DataFrame(data, columns=titles)
+    
+    @staticmethod
+    def printFormated(df: pd.DataFrame) -> None:
+        """
+        Imprime a tabela segundo a formatação requisitada pelo exercício.
+
+        ARGS:
+            df = Dataframe gerado pelo método numérico
+        """
+        print(df.to_string(index=False, justify='center'))
+
 
     def bisect(self) -> Tuple[float, pd.DataFrame]:
         """
         Calcula a aproximação da raiz da função pelo método da bisseção.
         """
+        # Variáveis iniciais
         a, b = self.x0, self.x1
-        it = self.maxit
         table_title = ['k', 'xk', 'f(xk)', 'step']
         table = []
-
-        for i in range(it):
+        # Loop principal
+        for i in range(self.maxit):
             step = abs(b - a)
+            # Bisseção
+            xk = (a + b) / 2
+            f_xk = self.func(xk)
+            table.append([i + 1, xk, f_xk, step])
+            # Lógica de redução
+            if (self.func(a) * f_xk < 0):
+                b = xk
+            else:
+                a = xk
+
             if (step < self.epsilon):
                 break
-            # Bisseção
-            c = (a + b) / 2
-            f_c = self.func(c)
-            table.append([i + 1, c, f_c, step])
-            # Lógica de redução
-            if (self.func(a) * f_c < 0):
-                b = c
-            else:
-                a = c
             
         full_table = self._table(table, table_title)
-        return f_c, full_table
+        return f_xk, full_table
 
     def newton(self) -> Tuple[float, pd.DataFrame]:
         """
         Calcula a aproximação da raiz da função pelo método de Newton.
         """
-        if (self.derivative is None):
-            raise ValueError("A derivada da função é necessária para o método de Newton.")
-        
-        table_title = ('k', 'xk', 'f(xk)', 'f\'(a)', 'step')
+        # Verificação inicial, a fim de evitar erro de divisão por None
+        if not callable(self.derivative):
+            raise ValueError("A derivada da função (derivative) não foi fornecida ou não é uma função.")
+        # Variáveis iniciais  
+        xk = self.x0
+        table_title = ['k', 'xk', 'f(xk)', 'f\'(xk)', 'step']
+        table = []
+        # Loop principal
+        for i in range(self.maxit):
+            _xk = xk
+            f_xk = self.func(xk)
+            f_dxk = self.derivative(xk)
+            xk = xk - (f_xk / f_dxk) # x_{k+1}
+            step = abs(xk - _xk)
+
+            table.append([i + 1, xk, self.func(xk), self.derivative(xk), step])
+
+        full_table = self._table(table, table_title)
+        return self.func(xk), full_table
 
     def secant(self) -> Tuple[float, pd.DataFrame]:
         """
         Calcula a aproximação da raiz da função pelo método da secante.
         """
-        table_title = ('k', 'xk', 'f(xk)', 'step')
-        pass
+        # Variáveis iniciais
+        xk0, xk1 = self.x0, self.x1
+        table = []
+        table_title = ['k', 'xk', 'f(xk)', 'step']
+        # Loop principal
+        for i in range(self.maxit):
+            f_xk0 = self.func(xk0)
+            f_xk1 = self.func(xk1)
+
+            den = xk0 * f_xk1 - xk1 * f_xk0
+            num = f_xk1 - f_xk0
+
+            xk0 = xk1 # x_k se torna x_{k-1}
+            xk1 = den / num # x_k se torna x_{k-1}
+            step = (xk1 - xk0)
+
+            table.append([i + 1, xk1, self.func(xk1), step])
+
+        full_table = self._table(table, table_title)
+        return self.func(xk1), full_table
